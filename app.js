07bf16f63f5a2b6d7b5e45ed4d46a5b5fb756d33
gica2902 <164004854+gica2902@users.noreply.github.com>
2024-10-07 11:33:07 -0300

    .

---
+++ app.js
@@ -0,0 +1,16 @@
+function criaCartao(categoria, pergunta, resposta){
+    let container = document.getElementsById('container');
+    let cartao = document.createElement('article');
+cartao.className = 'cartao';
+}
+
+cartao.innerHTML =
+<div class="cartao__conteudo>
+<h3> Programação</h3>
+<div class="cartao__conteudo__pergunta">
+    <p>O que é JavaScript </p>
+</div>
+<div class="cartao__conteudo__pergunta">
+O JavaScript é uma linguagem de Programação
+</div>
+</div>
