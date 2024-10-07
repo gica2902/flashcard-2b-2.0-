88ba3a6a80399e34e8d5f83c76a36e57c1caa0c8
gica2902 <164004854+gica2902@users.noreply.github.com>
2024-10-07 11:21:22 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,29 +1,29 @@
 criaCartao(
-'Conhecimentos Gerais'
-'Quem foi o primeiro homem a pisar na lua?'
+'Conhecimentos Gerais',
+'Quem foi o primeiro homem a pisar na lua?',
 'Neil Armstrong'
 )
 
 criaCartao(
-'Conhecimentos gerais'
-'Qual o maior país do mundo em área territorial?'
-'Rússia'
+'Conhecimentos gerais',
+'Qual o maior país do mundo em área territorial?',
+'Rússia',
 )
 
 criaCartao(
-'Conhecimentos gerais'
-'Quantos continentes existem no planeta terra?'
+'Conhecimentos gerais',
+'Quantos continentes existem no planeta terra?',
 'Sete'
 )
 
 criaCartao(
-'Conhecimentos gerais'
-'Quem escreveu "Dom Quixote"?'
-'Miguel de Cervantes'
+'Conhecimentos gerais',
+'Quem escreveu "Dom Casmurro"?',
+'Machado de Assis',
 )
 
 criaCartao(
-'Conhecimentos Gerais'
-'Qual o metal mais utilizado do mundo?'
-'O Ferro'
+'Conhecimentos Gerais',
+'Qual o metal mais utilizado do mundo?',
+'O Ferro',
 )
